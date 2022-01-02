e8d95c6784ee808dc5c0d550e88e0b757daca9a2
Miguel Lopes <lopesazevedom@gmail.com>
2022-01-02 16:29:56 +0000

    working on accepting project invites

--- resources/views/pages/userpage.blade.php
+++ resources/views/pages/userpage.blade.php
@@ -6,51 +6,55 @@
         {{Auth::user()->name}}'s Profile
     </div>
     <hr class = "edituserpageHR">
-    <div class = "BigDivEditProfile">
-        <div class ="goBack">
-            <form method="GET" action="{{'/deleteuser'}}" >
-                <input type="hidden" id= "deleteaccountButton" onchange="sofi(event)">
-                <button type="submit" class= "goBackButton" onclick="return confirm('Are you sure you want to delete your account?')" >Delete Account</button>
-            </form>
-        </div>
-        <div class = "userpageSetUp">
-            <div class = "textSpace">
-                <div class="userpageInfo">
-                    <p class="userName">{{ $user->name }}</p>
-                    <hr id = "userPageHR">
+
+    
+    <div class = "userpageSetUp">
+        <div class = "textSpace">
+            <div class="userpageInfo">
+                <p class="userName">{{ $user->name }}</p>
+                <hr id = "userPageHR">
+            </div>
+            <div class="userpageInfo">
+                <div id = "emailSpace">
+                    <i class="fas fa-envelope icon" id = "emailIcon" ></i>
+                    <p class="userEmail">{{ $user->email }}</p>
                 </div>
-                <div class="userpageInfo">
-                    <div id = "emailSpace">
-                        <i class="fas fa-envelope icon" id = "emailIcon" ></i>
-                        <p class="userEmail">{{ $user->email }}</p>
-                    </div>
+            </div>
+            @if (!is_null($user->profile_description))
+            <div class = "descriptionBox">
+                <div class = "userpageParam"> 
+                    <p class="userDescription">{{ $user->profile_description }}</p>
                 </div>
-                @if (!is_null($user->profile_description))
-                <div class = "descriptionBox">
-                    <div class = "userpageParam"> 
-                        <p class="userDescription">{{ $user->profile_description }}</p>
-                    </div>
-                </div>
-                @endif
-                <div id ="editProfileButton">
-                    <a href="{{ url('/edituserpage') }}" id = "editProfileButtonText">Edit Profile </a>
-                </div>
-            </div>  
-            <div class = "photoSpace">
-                <div id = "profilePhoto">
-                    <div id = "containerEditPhoto">
-                        <div class = "profilePhotoCropper">
-                            @if(empty(Auth::user()->profile_image))
-                                <img src = "/images/avatars/profile-pic-2.png" class = "roundPhoto" id = "tempProfilePhoto">
-                            @else
-                                <img src ="{{Auth::user()->profile_image}}" class = "roundPhoto" id = "tempProfilePhoto"> 
-                            @endif
-                        </div>
+            </div>
+            @endif
+
+            <div class="textSpaceButtons">
+                <a  class="profileButton" href="{{ url('/edituserpage') }}" id = "editProfileButtonText">Edit Profile </a>
+
+
+                <form method="GET" action="{{'/deleteuser'}}" >
+                    <input type="hidden" id= "deleteaccountButton" onchange="sofi(event)">
+                    <button type="submit" id="profileButtonDelete" class= "profileButton" >Delete Account</button>
+                </form>
+            </div>
+            
+
+        </div>  
+        <div class = "photoSpace">
+            <div id = "profilePhoto">
+                <div id = "containerEditPhoto" >
+                    <div class = "profilePhotoCropper">
+                        @if(empty(Auth::user()->profile_image))
+                            <img src = "/images/avatars/profile-pic-2.png" class = "roundPhoto" id = "tempProfilePhoto">
+                        @else
+                            <img src ="{{Auth::user()->profile_image}}" class = "roundPhoto" id = "tempProfilePhoto"> 
+                        @endif
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 </div>
 @endsection
 
