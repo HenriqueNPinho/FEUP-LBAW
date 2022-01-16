8db1eb2c01ac1bede24a6c497f4cbb3d8128b0d7
henrique <up201805000@up.pt>
2022-01-16 18:27:25 +0000

    About Us done

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -1,3 +1,15 @@
 
 @extends('layouts.app')
 @section('content')
+
+<div class="circle_container">
+	<div class="circle_main">
+		<div class="circle_text_container">
+			<div class = "circle_text">
+				<div class="circle_text_title"><p>About Us</p></div>
+                Project Clinic is the collaboration hub that brings the right tools together to get work done. 
+                Our main goal is to connect people to their teams, unify their systems, and drive their business forward.
+			</div>
+		</div>
+	</div>
+</div>
